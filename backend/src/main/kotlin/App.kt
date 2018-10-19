455fe12478ee2a0531709cc91f3adf4a2a4be104
stoimen01 <stoimen01@gmail.com>
2018-10-19 17:46:21 +0300

    Added backend project.

---
+++ backend/src/main/kotlin/App.kt
@@ -0,0 +1,16 @@
+import io.ktor.application.*
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            call.respondText("My Example Blog2", ContentType.Text.Html)
+        }
+    }
+}
